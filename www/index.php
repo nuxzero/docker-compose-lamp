da4dceeb033d7accc24ede5f78ececddd8b84cc7
Natthawut Hemathulin <natthawut1991@gmail.com>
2017-02-01 21:29:55 +0700

    Test database connecting.

--- www/index.php
+++ www/index.php
@@ -1,14 +1,15 @@
 <?php
- phpinfo();
+// phpinfo();
 
 //// Create connection
-//$host_db = "dockerdb";
-//$username = "root";
-//$password = "root";
-//$conn = new mysqli($host_db, $username, $password);
-//
-//// Check connection
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//}
-//echo "Connected successfully";
+$host_db = "dockerdb";
+$username = "root";
+$password = "root";
+$database = "database";
+$conn = new mysqli($host_db, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
